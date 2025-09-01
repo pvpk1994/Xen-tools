62c8c05c99eca00f7a71cf10c32722759e96bff3
Pavan Kumar Paluri <pvpk1994@gmail.com>
2025-09-01 17:11:16 +0000

    Add kernel and ramdisk images to VM configuration file
    The VM configuration file generated by xen-create-image enables
    PyGrub bootloader which is not desirable in cases where custom kernel
    needs to be selected for VM's guest OS.
    
    Add support to vm_configure script to automatically disable PyGrub
    bootloader and add user-selected kernel and ramdisk images to the
    xen-create-image generated VM configuration file.
    
    Signed-off-by: Pavan Kumar Paluri <pvpk1994@gmail.com>

--- vm_configure.sh
+++ vm_configure.sh
@@ -30,6 +30,38 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+update_vm_cfg() {
+	local VM_NAME="$1"
+	local KERN_IMG="$2"
+	local INITRD_IMG="$3"
+	local CFG_FILE="/etc/xen/${VM_NAME}.cfg"
+
+	# Check if xen-create-image succeeded or not
+	if [[ $? -ne 0 ]]; then
+		echo "xen-create-image failed. Skipping ${VM_NAME}.cfg updates"
+		return 2
+	fi
+
+	# If xen-create-image succeeds, check for vm_name.cfg validity
+	if [[ ! -f "$CFG_FILE" ]]; then
+		echo "VM config file not found: $CFG_FILE"
+		return 2
+	fi
+
+	# Disable bootloader (PyGrub) option
+	sed -i '/^bootloader *=/s/^/#/' "$CFG_FILE"
+
+	# Add kernel and initrd images to VM.cfg, if not present
+	if  grep -q '^kernel' "$CFG_FILE" || grep -q '^ramdisk' "$CFG_FILE"; then
+		echo "kernel and ramdisk images already present"
+		return 2
+	else
+		echo "kernel = \"$KERN_IMG\"" >> "$CFG_FILE"
+		echo "ramdisk = \"$INITRD_IMG\"" >> "$CFG_FILE"
+		echo "$CFG_FILE updated with kernel and initrd images"
+	fi
+}
+
 echo "Choose an option:"
 echo "1. Create VM config with xen-create-image"
 echo "2. Erase VM config with xen-delete-image"
@@ -40,7 +72,7 @@ if [[ "$choice" == "1" ]]; then
 
 	read -p "Enter name of the VM: " VM_NAME
 	read -p "Enter RAM size of the VM image (in MB): " VM_SIZE
-	read -p "Kernel Image: " KERN_IMG
+	read -p "Kernel Image (vmlinuz): " KERN_IMG
 	read -p "INITRD image: " INITRD_IMG
 
 	# Cmdline args validations
@@ -79,6 +111,9 @@ if [[ "$choice" == "1" ]]; then
 		--initrd="$INITRD_IMG"			\
 		--password="$VM_NAME"			\
 		--force
+
+	# Once here, configuration file should have been successfully generated
+	update_vm_cfg "$VM_NAME" "$KERN_IMG" "$INITRD_IMG"
 
 elif [[ "$choice" == "2" ]]; then
 	echo "Option 2 selected. Running ./vm_configure.sh for option 2"
