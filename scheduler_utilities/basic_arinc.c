70cd3d2f9bc0a233969350e49f1a8a6e07257b8a
Pavan Kumar Paluri <pvpk1994@gmail.com>
2025-09-16 01:27:05 +0000

    Scheduler utility support for ARINC-653 VM scheduler
    Introduce support for scheduling all the DomUs that are assigned to
    ARINC-653 scheduler operated cpupool. The initial attempt is to create
    a non-preemptive scheduler that will schedule all the DomUs based on
    the WCET(Worst Case Execution Time) assigned to them.
    
    The utility also helps ensure the WCET for a given DomU does not exceed
    its period. If otherwise, the utility reports an error and declines to
    set the schedule for ARINC-653.
    
    Besides, if the cumulative WCETs of all ARINC-operated DomUs fall short
    of the hyperperiod, then idle time slices take over and run for the
    remainder of the time-period until next major frame begins.
    
    TODOs:
    - The schedule generated is not yet delivered to the hypervisor via
      hypercall just yet. Once delivered, ARINC-653 will schedule the DomUs
      based on the schedule set by this utility.
    
    - ARINC-653 scheduler does not support multi-vCPU implementation. Extend
      support for it later by bringing in Hierarchial Real time systems
      Partitioning - Regularity based Resource Partitioning Model (HiRTS-RRP)
    
    Signed-off-by: Pavan Kumar Paluri <pvpk1994@gmail.com>

--- scheduler_utilities/basic_arinc.c
+++ scheduler_utilities/basic_arinc.c
@@ -13,11 +13,14 @@
 #include <xenctrl.h>    /* For obtaining ARINC 653 scheduler info */
 #include <libxl.h>      /* For obtaining DomU information */
 #include <uuid/uuid.h> /* For parsing DomU's handle */
+#include <libxl_utils.h> /* For libxl bitmap operations */
 
 #define MILLISECS(x)		((x) * 1000000ULL)
 #define DEFAULT_TIMESLICE	MILLISECS(10)
 
 struct dom_attr {
+	libxl_uuid uuid;
+	libxl_domid domid;
 	int wcet;
 	int period;
 };
@@ -25,11 +28,6 @@ struct dom_attr {
 int gcd(int a, int b) { return (b == 0) ? a : gcd(b, a % b); }
 
 int lcm(int a, int b) { return (a * b)/gcd(a,b); }
-
-void copy_uuid(xen_domain_handle_t dest, xen_domain_handle_t src)
-{
-	memcpy(dest, src, sizeof(xen_domain_handle_t));
-}
 
 int calculate_hyperperiod(struct dom_attr *attrs, int count)
 {
@@ -43,17 +41,21 @@ int calculate_hyperperiod(struct dom_attr *attrs, int count)
 	return result;
 }
 
-void compute_schedule(struct dom_attr *attrs, int hp, int count,
-		      struct xen_sysctl_arinc653_schedule *sched,
-		      libxl_dominfo *info)
+void compute_schedule(libxl_ctx *ctx, struct dom_attr *attrs, int hp, int count,
+		      struct xen_sysctl_arinc653_schedule *sched)
 {
+	libxl_uuid idle_dom;
+	memset(&idle_dom, 0, sizeof(idle_dom));
+
 	sched->major_frame = hp;
+	printf("Hyperperiod obtained is: %ld\n", sched->major_frame);
 
 	for (int dom =0; dom < count; dom++) {
 		for (int wcet = 0; wcet < attrs[dom].wcet; wcet++) {
 			sched->sched_entries[sched->num_sched_entries].runtime = DEFAULT_TIMESLICE;
-			copy_uuid(sched->sched_entries[sched->num_sched_entries].dom_handle,
-				  info[dom].uuid.uuid);
+
+			memcpy(&sched->sched_entries[sched->num_sched_entries].dom_handle,
+			       &attrs[dom].uuid, sizeof(libxl_uuid));
 
 			/* TODO: when multi-vCPU support is introduced, this needs change */
 			sched->sched_entries[sched->num_sched_entries].vcpu_id = 0;
@@ -74,7 +76,12 @@ void compute_schedule(struct dom_attr *attrs, int hp, int count,
 	/* Consume up the remaining time-slices with idle entries */
 	for (int entry = sched->num_sched_entries; entry < sched->major_frame; entry++) {
 		sched->sched_entries[entry].runtime = DEFAULT_TIMESLICE;
-		copy_uuid(sched->sched_entries[entry].dom_handle, (xen_domain_handle_t){0xff});
+
+		/* For idle schedule-entry's domain handle, use Domain-0's handle as a
+		 * placeholder. This is a safe operation because the arinc cpupool will
+		 * never have a Domain-0 included in it's schedule.
+		 */
+		memcpy(&sched->sched_entries[entry].dom_handle, &idle_dom, sizeof(libxl_uuid));
 	}
 
 	return;
@@ -100,26 +107,79 @@ int main()
 	struct dom_attr *dom_attrs;
 	libxl_ctx *ctx;
 	libxl_dominfo *info;
-	int nb_domain, hp;
+	libxl_cpupoolinfo *pools, arinc_pool;
+	int nb_domain, hp, npools, total_doms, arinc_dom_index=0;
 
 	if (libxl_ctx_alloc(&ctx, LIBXL_VERSION, 0, NULL) != 0) {
 		printf("Failed to allocate libxl_ctx_allocate");
 		return -1;
 	}
 
-	info = libxl_list_domain(ctx, &nb_domain);
-	if (!info) {
-		printf("libxl_list_domain failed.\n");
+	/* Get cpupool information */
+	pools = libxl_list_cpupool(ctx, &npools);
+
+	#ifdef ENABLE_DUMP
+	printf("Number of cpupools: %d\n", npools);
+	#endif
+
+	libxl_cpupoolinfo_init(&arinc_pool);
+
+	for (int i=0; i < npools; i++) {
+		if (strcmp(pools[i].pool_name, "arinc_pool") == 0) {
+			arinc_pool.pool_name = strdup(pools[i].pool_name);
+
+			#ifdef ENABLE_DUMP
+			printf("POOL NAME: %s\n", arinc_pool.pool_name);
+			#endif
+
+			if (pools[i].cpumap.size == 0) {
+				fprintf(stderr, "Invalid cpumap size: 0\n");
+				return -1;
+			}
+
+			libxl_bitmap_copy_alloc(ctx, &arinc_pool.cpumap, &pools[i].cpumap);
+
+			#ifdef ENABLE_DUMP
+			printf("arinc_pool.cpumap.size: 0x%x\n", arinc_pool.cpumap.size);
+			printf("pool[i].cpumap.size: 0x%x\n", pools[i].cpumap.size);
+			#endif
+
+			arinc_pool.poolid = pools[i].poolid;
+			arinc_pool.sched = pools[i].sched;
+			arinc_pool.n_dom = pools[i].n_dom;
+			break;
+		}
+	}
+
+
+	dom_attrs = malloc(arinc_pool.n_dom * sizeof(*dom_attrs));
+	if (!dom_attrs) {
+		fprintf(stderr, "malloc failure\n");
 		return -1;
 	}
 
-	dom_attrs = malloc(sizeof(struct dom_attr) * nb_domain);
+	/* Get domain information */
+	info = libxl_list_domain(ctx, &total_doms);
 
-	for (int i=0; i < nb_domain; i++) {
-		printf("Enter WCET for Domain-%d: ", info[i].domid);
+	for (int i=0; i < total_doms; i++) {
+		if (info[i].cpupool == arinc_pool.poolid) {
+
+			if (arinc_dom_index >= arinc_pool.n_dom) {
+				fprintf(stderr, "More domains expected in cpupool\n");
+				break;
+			}
+
+			dom_attrs[arinc_dom_index].domid = (uint32_t)info[i].domid;
+			libxl_uuid_copy(ctx, &dom_attrs[arinc_dom_index].uuid, &info[i].uuid);
+			arinc_dom_index++;
+		}
+	}
+
+	for (int i=0; i < arinc_pool.n_dom; i++) {
+		printf("Enter WCET for Domain-%d: ", dom_attrs[i].domid);
 		scanf("%d", &dom_attrs[i].wcet);
 
-		printf("Enter period for Domain-%d: ", info[i].domid);
+		printf("Enter period for Domain-%d: ", dom_attrs[i].domid);
 		scanf("%d", &dom_attrs[i].period);
 
 		if (dom_attrs[i].period < dom_attrs[i].wcet) {
@@ -128,10 +188,10 @@ int main()
 		}
 	}
 
-	hp = calculate_hyperperiod(dom_attrs, nb_domain);
+	hp = calculate_hyperperiod(dom_attrs, arinc_pool.n_dom);
 	printf("Hyperperiod is: %d\n", hp);
 
-	compute_schedule(dom_attrs, hp, nb_domain, &sched, info);
+	compute_schedule(ctx, dom_attrs, hp, arinc_pool.n_dom, &sched);
 
 #ifdef ENABLE_DUMP
 	dump_schedule(sched);
